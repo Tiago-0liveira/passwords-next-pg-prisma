9b1c4b4d3f180cf9acf16e9f0f812f2469e2429d
Tiago-0liveira <tfgoliveira@gmail.com>
2021-04-08 15:10:54 +0100

    changed to ts

---
+++ pages/api/hello.ts
@@ -0,0 +1,9 @@
+import {NextApiRequest, NextApiResponse} from "next"
+
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+
+export default (_req: NextApiRequest, res: NextApiResponse) => {
+	res.status(200).json({ name: "John Doe" });
+};
