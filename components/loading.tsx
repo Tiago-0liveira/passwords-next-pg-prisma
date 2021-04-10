a529fa19a6a84bb5dbb8b5c6b5d8902f3f83ee27
Tiago-0liveira <tfgoliveira@gmail.com>
2021-04-10 21:16:56 +0100

    fixed warning for no name () => {}

--- components/loading.tsx
+++ components/loading.tsx
@@ -3,6 +3,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircleNotch } from '@fortawesome/free-solid-svg-icons'
 
 
-export default () => {
+const Loading = () => {
     return <FontAwesomeIcon icon={faCircleNotch} spin size={"5x"} color={"#09203f"} />
 }
+
+export default Loading
