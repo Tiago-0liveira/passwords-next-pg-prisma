ff20863c4b930beb92a7d890c4ab3831a60b0ae0
Tiago-0liveira <tfgoliveira@gmail.com>
2021-04-10 01:09:07 +0100

    token auth

---
+++ pages/api/token.ts
@@ -0,0 +1,21 @@
+import type { NextApiResponse } from 'next'
+import { User } from "@prisma/client"
+import { CustomApiRequest, IApiBodyToken, IApiResToken } from '../../@types/types'
+import prisma from "../../prisma/db"
+
+export default async (req: CustomApiRequest<IApiBodyToken>, res: NextApiResponse<IApiResToken>) => {
+    if (req.method === "POST") {
+        const user: User | null = await prisma.user.findFirst({
+            where: {
+                loginToken: req.body.token
+            }
+        })
+        if (user) {
+            res.status(200).json({ success: true, user })
+        } else {
+            res.status(400).json({ success: false, error: "Token not valid" })
+        }
+    } else {
+        res.status(404).json({ success: false, error: `${req.method.toUpperCase()}` })
+    }
+}
