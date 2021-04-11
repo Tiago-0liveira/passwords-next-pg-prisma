3c1e2fe202f717d3edeadafe67a9af9efe70ca5d
Tiago-0liveira <tfgoliveira@gmail.com>
2021-04-11 01:26:57 +0100

    redirect if logged in and basically finnished

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,27 @@
 import Head from "next/head"
-import { v4 } from "uuid"
 import useUser from "../components/hooks/useUser"
-
+import styles from "../styles/index.module.scss"
+import LoadingSvg from "../components/loading"
+import LogInForm from "../components/LogInForm"
+import { useRouter } from "next/router"
 
 export default function Home() {
-    const [user, loading] = useUser()
+    const [user, loading, setUser] = useUser()
+    const router = useRouter()
 
+    if (user) {
+        router.push("/app")
+    }
     return (
-        <div>
+        <div className={styles.wrapper}>
             <Head>
                 <title>Home</title>
             </Head>
+
+
+            <div className={styles.content}>
+                {loading ? <LoadingSvg /> : <LogInForm />}
+            </div>
         </div>
     )
 }
