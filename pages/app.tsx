37e8ac25b161e0aea24dd53854ddc9b5161d3683
Tiago-0liveira <tfgoliveira@gmail.com>
2021-04-12 12:23:07 +0100

    main app.tsx working (still has things missing)

--- pages/app.tsx
+++ pages/app.tsx
@@ -2,33 +2,103 @@ import Link from "next/link"
 import styles from "../styles/app.module.scss"
 import LogInStyles from "../styles/components.LogInForm.module.scss"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowLeft, faPlus } from '@fortawesome/free-solid-svg-icons'
-const rows = []
+import { faArrowDown, faArrowLeft, faPlus, faUser } from '@fortawesome/free-solid-svg-icons'
+import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react"
+import { Row } from ".prisma/client"
+import Loading from "../components/loading"
+import RowComponent from "../components/row"
+import { HEADERS, SortState } from "../constants/consts"
+import useUser from "../components/hooks/useUser"
+import { User } from "@prisma/client"
+import useWBOL from "../components/hooks/useWBOL"
 
 const App = () => {
+    const [user] = useUser()
+    const [rows, setRows] = useState<false | Row[]>(false)
+    const [focus, setFocus] = useState("")
+    const [sortState, setsortState] = useState<SortState>(SortState.Site)
+    const [inputVal, setinputVal] = useState("")
+    const [WBOL, setWBOL] = useWBOL();
+
+    useEffect(() => {
+        user && fetch("/api/row", {
+            method: "POST",
+            headers: HEADERS,
+            body: JSON.stringify({
+                type: "get",
+                userId: (user as User).id
+            })
+        }).then(res => res.json()).then((data: Row[]) => {
+            if (data) {
+                setRows(data)
+            } else {
+                console.log("app.tsx|33|: Data podre")
+            }
+        })
+    }, [user]);
+
     return (
         <div className={[styles.Wrapper].join(" ")}>
-            <nav className={[styles.flex].join(" ")}>
-                <Link href="/">
-                    <button className={[LogInStyles.button, styles.buttonBack].join(" ")}>
-                        <FontAwesomeIcon icon={faArrowLeft} />
+            <nav className={[styles.nav].join(" ")}>
+                <div className={styles.navLeft}>
+                    <Link href="/" >
+                        <button className={[LogInStyles.button, styles.buttonBack].join(" ")} onClick={() => {
+                            (setWBOL as Dispatch<SetStateAction<boolean>>)(true)
+                        }}>
+                            <FontAwesomeIcon icon={faArrowLeft} />
                         Back
                     </button>
-                </Link>
-                <button className={[LogInStyles.button, styles.buttonNewRow].join(" ")}>
-                    <FontAwesomeIcon icon={faPlus} />
-                    New Row
-                </button>
+                    </Link>
+                    <button className={[LogInStyles.button, styles.buttonNewRow].join(" ")}>
+                        <FontAwesomeIcon icon={faPlus} />
+                        New Row
+                    </button>
+                </div>
+                <div className={styles.navMiddle}>
+                    <div className={styles.inputWrapper}>
+                        <input type="text" placeholder="Search" onLoad={(e) => { console.log(e) }} onChange={(e) => setinputVal(e.target.value)} />
+                    </div>
+                </div>
+                <div className={styles.navRight}>
+                    <button className={[LogInStyles.button, styles.buttonNewRow].join(" ")}>
+                        <FontAwesomeIcon icon={faUser} />
+                        {user?.username}
+                    </button>
+                </div>
             </nav>
             <div className={[styles.wrapper, styles.flex].join(" ")}>
                 <div className={styles.content}>
                     <div className={styles.header}>
-                        Site | Name | Password
+                        <div>
+                            <div>
+                                Site
+                            </div>
+                        </div>
+                        <div>
+                            <div>
+                                Name
+                            </div>
+                        </div>
+                        <div>
+                            <div>
+                                Password
+                            </div>
+                        </div>
                     </div>
                     <div className={styles.rows}>
-                        {rows.map(row => {
-                            <Row props={row}/>
-                        })}
+                        {rows ?
+                            (rows as Row[]).sort((a: Row, b: Row) =>
+                                a.site.toLowerCase() > b.site.toLowerCase() ? 1 : -1
+                            ).filter((v: Row) =>
+                                v.site.includes(inputVal) ||
+                                v.email.includes(inputVal) ||
+                                v.username?.includes(inputVal)
+                            ).map((row: Row, i) =>
+                                <RowComponent {...row} key={i} onClick={(e: MouseEvent) => {
+
+                                }} />
+                            )
+                            : <Loading />}
                     </div>
                 </div>
             </div>
