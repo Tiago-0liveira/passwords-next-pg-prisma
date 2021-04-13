8307058ef93bedb2809cc1166f3326d31ad1e561
Tiago-0liveira <tfgoliveira@gmail.com>
2021-04-13 09:55:50 +0100

    now using scss and variables have their file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from 'next/app';
-import "../styles/styles.scss"
+import "../styles/styles.css"
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
